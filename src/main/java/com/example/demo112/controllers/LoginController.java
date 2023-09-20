8810b0dcb30c04d2d9d9326aae3de0dd1b86d656
QuocDuy <nguyendqduy@gmail.com>
2023-09-20 13:18:07 +0700

    master1

--- src/main/java/com/example/demo112/controllers/LoginController.java
+++ src/main/java/com/example/demo112/controllers/LoginController.java
@@ -9,32 +9,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import com.example.demo112.models.User;
+
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Xử lý yêu cầu GET tại đây
+        // Ví dụ: Hiển thị form đăng nhập
+        String url = "/WEB-INF/views/index.html";
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-
-
         String firstname = request.getParameter("firstName");
         String lastname = request.getParameter("lastName");
         String email = request.getParameter("email");
         User user = new User(lastname, firstname, email);
 
         request.setAttribute("user", user);
-        String url = "/login.jsp";
-
-
-        getServletContext().getRequestDispatcher(url).forward(request, response);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String url = "/index.html"; // URL mặc định cho yêu cầu GET
-
-        // Xử lý yêu cầu GET tại đây (nếu cần)
+        String url = "/WEB-INF/views/login.jsp";
 
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
+
+    // Rest of the code for doPost and other methods if needed
 }
+
+
