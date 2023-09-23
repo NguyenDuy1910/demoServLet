7016e4043ec4d5b4db7ff1d4da7da705b992af6b
QuocDuy <nguyendqduy@gmail.com>
2023-09-23 23:01:14 +0700

    abc

--- src/main/java/com/example/demo112/controllers/LoginController.java
+++ src/main/java/com/example/demo112/controllers/LoginController.java
@@ -1,64 +1,52 @@
 package com.example.demo112.controllers;
-
 import java.io.IOException;
-
-import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.example.demo112.models.User;
 
-//@WebServlet("/login")
+@WebServlet("/login")
 public class LoginController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doPost(request, response);
-    }
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String url = "/index.html";
-//        PrintWriter out = response.getWriter();
+
         String action = request.getParameter("action");
 
-        if(action == null)
-            action="join";
+        if (action == null)
+            action = "join";
 
-        if(action.equals("join"))
+        if (action.equals("join"))
             url = "/index.html";
-        else if (action.equals("add")){
-        String firstname =  request.getParameter("firstName");
-        String lastname =  request.getParameter("lastName");
-        String email =  request.getParameter("email");
-        User user = new User(lastname, firstname, email);
 
-        user.setLastName(lastname);
-        user.setFirstName(firstname);
-        user.setEmail(email);
-        request.setAttribute("user", user);
-        url = "/index.jsp";
+        else if (action.equals("add")) {
+            String firstname = request.getParameter("firstName");
+            String lastname = request.getParameter("lastName");
+            String email = request.getParameter("email");
+            User user = new User(lastname, firstname, email);
+
+            request.setAttribute("user", user);
+            url = "/index.jsp";
+        }
+
+        getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
-    getServletContext().getRequestDispatcher(url).forward(request, response);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = "/index.html";
+
+        String action = request.getParameter("action");
+
+        if (action == null)
+            action = "join";
+
+        if (action.equals("join"))
+            url = "/index.html";
+
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
 }
-//        boolean status = bean.validate();
-//
-//        if (status) {
-//            RequestDispatcher rd = request
-//                    .getRequestDispatcher("login-success.jsp");
-//            rd.forward(request, response);
-//        } else {
-//            RequestDispatcher rd = request
-//                    .getRequestDispatcher("login-error.jsp");
-//            rd.forward(request, response);
-//        }
-
-    }
-
-    // Rest of the code for doPost and other methods if needed
-
-
-
