cff51cee9042b2b85960c907e16ed6671a42b0b2
QuocDuy <nguyendqduy@gmail.com>
2023-10-11 15:06:56 +0700

    new commit 11/10/2023

--- src/main/java/com/example/demo112/controllers/TestServlet.java
+++ src/main/java/com/example/demo112/controllers/TestServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
 
 
 @WebServlet("/test")
@@ -13,8 +15,21 @@ public class TestServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Hiển thị trang web (form) cho phương thức GET
+        response.setContentType("text/html");
 
-        request.getRequestDispatcher("form.html").forward(request, response);
+        // Lấy PrintWriter từ đối tượng response
+        PrintWriter out = response.getWriter();
+
+        // Đoạn văn bạn muốn hiển thị
+        String paragraph = "Đây là một ví dụ về việc hiển thị một câu văn bằng Servlet.";
+
+        // Ghi đoạn văn vào phản hồi
+        out.println(paragraph);
+
+        // Đảm bảo rằng bạn đã đóng PrintWriter
+        out.close();
+
+//        request.getRequestDispatcher("form.html").forward(request, response);
     }
 
 
