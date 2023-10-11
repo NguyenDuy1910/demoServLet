c2118a0815b64c60fb9d284dda706aab575f4bb8
QuocDuy <nguyendqduy@gmail.com>
2023-10-11 17:05:50 +0700

    new commit 11/10/2023

--- src/main/java/com/example/demo112/jdbc/JDBCConnection.java
+++ src/main/java/com/example/demo112/jdbc/JDBCConnection.java
@@ -6,10 +6,11 @@ import java.sql.SQLException;
 
 public class JDBCConnection {
 
-    private static final String URL = "jdbc:mariadb://localhost:3306/shopapp";
-    private static final String DRIVER = "org.mariadb.jdbc.Driver";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // Đảm bảo sử dụng driver của MySQL
+    private static final String USERNAME = "vs7gi59do10b9t0v"; // Tên người dùng từ URL
+    private static final String PASSWORD = "k16wgjwovj4wsy7q"; // Mật khẩu từ URL
 
     public Connection getConnection() {
         Connection connection = null;
